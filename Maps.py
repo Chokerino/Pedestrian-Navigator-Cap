b6e5f9a37c8e84044fc3933f5140bafd8e269b9a
Chokerino <43617111+Chokerino@users.noreply.github.com>
2019-04-08 14:28:26 +0530

    initial python script

--- Maps.py
+++ Maps.py
@@ -1,8 +1,13 @@
 import openrouteservice
 api="5b3ce3597851110001cf6248f05149ce402b4633920433f30aab79df"
-coords = ((77.107374,28.530765),(77.109903,28.532033))
+coords = ((77.268126, 28.545782),(77.26976,28.546887))
 
 client = openrouteservice.Client(key='5b3ce3597851110001cf6248f05149ce402b4633920433f30aab79df') # Specify your personal API key
-routes = client.directions(coords,profile="foot-walking",geometry_simplify=True,format="geojson")
+routes = client.directions(coords,profile="foot-walking",format="geojson")
 
-print (routes)
+current = routes["features"][0]["properties"]["segments"][0]["steps"][0]
+
+curr_instruct=current["instruction"]
+curr_type=current["type"]
+
+print(curr_instruct,curr_type)
